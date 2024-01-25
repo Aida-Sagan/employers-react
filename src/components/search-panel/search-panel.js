e7440f4fdced9015f548aad93e01220737f241e9
Aida <aida2001@mail.ru>
2024-01-25 08:07:54 +0600

    added the website layout

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+import './search-pannel.css';
+
+const SearchPanel = () => {
+    return (
+        <input type="text"
+               className="form-control search-input"
+               placeholder="Find the employer"/>
+    )
+}
+
+export default SearchPanel;
